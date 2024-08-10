6da1fd3d9be68853d94ded9b02f9c245695f4cd1
José Junquera <jose.junquera@esgalla.com>
2024-08-10 11:43:17 +0200

    Initial commit: Setup project structure and added basic configuration

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="container">
+      <h1 className="container py-24 text-center text-2xl font-bold lg:text-4xl">
+        Next.js movies demo app
+      </h1>
+    </main>
+  );
+}
